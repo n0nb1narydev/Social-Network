ed2fdf85207f670f6e9fc42346c663190bfa5ac2
Mel Rumsey <melsrumsey@gmail.com>
2020-10-09 14:05:56 -0500

    log out created

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask_bcrypt import check_password_hash 
-from flask_login import LoginManager, login_user
+from flask_login import (LoginManager, login_user, logout_user,
+                        login_required)
 
 import forms
 import models
@@ -70,6 +71,13 @@ def login():
                 flash("Your email or password doesn't match!", "error")
         return render_template('login.html', form=form)
 
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()   # deletes the login user cookie
+    flash("You've been logged out.", "success")
+    return redirect(url_for('index'))
 
 @app.route('/')
 def index():
